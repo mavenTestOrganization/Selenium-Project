6d1fe5e37bafa99fa7614c38690aa5f76aef842b
borhanMorphy <borhano.f.42@gmail.com>
2018-01-10 18:42:34 +0200

    update /pom.xml

--- src/main/java/com/seleniumProject/helper/CrossBrowserHandler.java
+++ src/main/java/com/seleniumProject/helper/CrossBrowserHandler.java
@@ -3,10 +3,56 @@
  */
 package com.seleniumProject.helper;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 /**
- * @author mchtctn
+ * This class provides multiple browser usage
  *
  */
 public class CrossBrowserHandler {
+	static WebDriver driver;
 
+	/**
+	 * This function runs the browser with given browser and URL
+	 * @param browser as String
+	 * @param URL as String
+	 * @return WebDriver object
+	 * 
+	 */
+	public static WebDriver startBrowser(String browser,String URL) {
+
+		if( browser.equalsIgnoreCase("firefox") ) {
+
+			System.setProperty("webdriver.gecko.driver",
+					System.getProperty("user.dir")+"src\\main\\drivers\\geckodriver.exe");
+
+			driver = new FirefoxDriver();
+
+		}
+		else if( browser.equalsIgnoreCase("chrome") ) {
+
+			System.setProperty("webdriver.chrome.driver",
+					System.getProperty("user.dir")+"src\\main\\drivers\\chromedriver.exe");
+
+			driver = new ChromeDriver();
+
+		}
+		else {
+			// otherwise return null
+
+			System.out.println("[SYSTEM] Given browser not found!");
+
+			return null;
+
+		}
+
+		driver.manage().window().maximize();
+
+		driver.get(URL);
+
+		return driver;
+
+	}
 }
--- src/test/java/com/seleniumProject/imdb/Pages/HomePage.java
+++ src/test/java/com/seleniumProject/imdb/Pages/HomePage.java
@@ -1,5 +1,94 @@
 package com.seleniumProject.imdb.Pages;
 
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.FindBys;
+import org.openqa.selenium.support.PageFactory;
+
 public class HomePage {
+
+	WebDriver driver;
+
+	@FindBy(xpath = "//*[@id=\"navbar-query\"]")
+	WebElement searchBox;
+
+	@FindBy(xpath = "//*[@id=\"navbar-submit-button\"]/div")
+	WebElement searchButton;
+
+	@FindBy(xpath = "//*[@id=\"quicksearch\"]")
+	WebElement searchReference;
+
+	@FindBy(xpath = "//*[@id=\"navMenu1\"]/div[2]/ul[1]/li[6]/a")
+	WebElement showTopRatedMovies;
+
+	@FindBys({
+		@FindBy(xpath = "//*[@id=\\\"navbar-suggestionsearch\\\"]/a[0]/div"),
+		@FindBy(xpath = "//*[@id=\\\"navbar-suggestionsearch\\\"]/a[1]/div"),
+		@FindBy(xpath = "//*[@id=\\\"navbar-suggestionsearch\\\"]/a[2]/div")
+	})
+	List<WebElement> firstThreeSuggesstions;
+
+
+	public HomePage(WebDriver driver) {
+
+		this.driver = driver;
+
+		//This initElements method will create all WebElements
+
+		PageFactory.initElements(driver, this);
+
+	}
+	/**
+	 * 
+	 * @param
+	 * 
+	 * @return
+	 */
+	public void goToTopRatedMovies() {
+
+		showTopRatedMovies.click();
+
+	}
+
+
+	public void setTextToSeachBox(String keyWords) {
+
+		searchBox.clear();
+
+		searchBox.sendKeys(keyWords);
+
+	}
+
+	/**
+	 * This function types the given input to searchBox and
+	 * finds the suggestions
+	 * 
+	 * @param keyWords
+	 * 
+	 * @return String array that collected from List<WebElements>
+	 */
+	public String[] getFirstThreeSuggesstions(String keyWords) {
+
+		this.setTextToSeachBox(keyWords);
+
+		String[] myList = new String[firstThreeSuggesstions.size()];
+
+		for(int i = 0 ; i < myList.length ; i++)
+			myList[i] = firstThreeSuggesstions.get(i).getText();
+
+		return myList;
+	}
+	/**
+	 *
+	 * @param reference
+	 */
+	public void setSearchReference(String reference) {
+		
+		searchReference.sendKeys(reference);
+		
+	}
 
 }
