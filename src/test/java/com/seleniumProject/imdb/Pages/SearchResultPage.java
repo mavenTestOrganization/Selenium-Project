1045b26ab5182c70e4cd22dc66a1c8ec3cdb74b4
borhanMorphy <borhano.f.42@gmail.com>
2018-01-10 19:35:23 +0200

    update /SearchResultPage.java

--- src/test/java/com/seleniumProject/imdb/Pages/SearchResultPage.java
+++ src/test/java/com/seleniumProject/imdb/Pages/SearchResultPage.java
@@ -1,5 +1,41 @@
 package com.seleniumProject.imdb.Pages;
 
-public class SearchResultPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+/**
+ * 
+ * This class is a description for result page
+ * due to homePage's search box execution
+ *
+ */
+public class SearchResultPage {
+	WebDriver driver;
+
+	@FindBy(xpath = "//*[@id=\"main\"]/div/div[2]/table/tbody/tr[1]/td[2]/a")	
+	WebElement firstResultFromName;
+
+	public SearchResultPage(WebDriver driver) {
+
+		this.driver = driver;
+
+		//This initElements method will create all WebElements
+
+		PageFactory.initElements(driver, this);
+
+	}
+	
+	/**
+	 * This function clicks the first text-link from the list
+	 * 
+	 */
+	public void clickFirstResultFromName() {
+		
+		firstResultFromName.click();
+		
+	}
+	
+	
 }
