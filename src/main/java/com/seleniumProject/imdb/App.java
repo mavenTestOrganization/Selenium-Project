2060f3d2bceaeb6465be3a1abf9ee452607b5861
borhanMorphy <borhano.f.42@gmail.com>
2018-01-10 16:42:58 +0200

    update

--- src/main/java/com/seleniumProject/imdb/App.java
+++ src/main/java/com/seleniumProject/imdb/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("asdasdad");
         System.out.println("text");
+        System.out.println("memati");
     }
 }
