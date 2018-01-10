1d4e495aca75ffd8d00bf1cae061f53638b3bba4
borhanMorphy <borhano.f.42@gmail.com>
2018-01-10 16:41:32 +0200

    commit2

--- src/test/java/com/seleniumProject/imdb/AppTest.java
+++ src/test/java/com/seleniumProject/imdb/AppTest.java
@@ -17,8 +17,7 @@ public class AppTest
      */
     public AppTest( String testName )
     {
-        super( testName );
-        System.out.println("this is a test asdfşlsadj"); 
+        super( testName ); 
     }
 
     /**
